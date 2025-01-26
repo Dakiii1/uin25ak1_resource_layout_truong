6ca16e8bf1f7c0df636f3ba61d2159cbcc985cdd
Dakiii1 <111729520+Dakiii1@users.noreply.github.com>
2025-01-26 11:24:04 +0100

    progress
    Gjort selve HTML del, startet litt på CSS og Javascript. tar en liten break begrunn av hjernen mental boomer c:

---
+++ funksjon.js
@@ -0,0 +1,11 @@
+console.log(resources)
+console.log(resources[0].category)
+
+const navHTML = document.getElementById("navigate")
+
+let navigateList = ""
+function navResourceList(category, index) {
+    navigateList += `<li id="nav${index}" onClick="(showHTML(${index}))"><span id="nav${index}span">${category.category}</span></li>`
+}
+resources.forEach(navResourceList)
+navHTML.innerHTML = navigateList
